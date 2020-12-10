a6e53aa679c8573e13baa56101d3c884feba1b7d
William Schutte <williamdschutte@gmail.com>
2020-12-09 21:07:17 -0500

    Functional search

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Scripts responsible for web scraping
 const search_jobs = require('./middleware/search_jobs');
@@ -8,8 +9,9 @@ const return_jobs = require('./middleware/return_jobs');
 
 const app = express()
 
+app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/search', search_jobs, scrape_jobs, return_jobs);
+app.post('/search', search_jobs, scrape_jobs, return_jobs);
 
 app.listen(5000, () => console.log('Server listening on Port 5000'));
